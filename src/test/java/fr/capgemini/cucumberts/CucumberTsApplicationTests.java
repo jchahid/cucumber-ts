a13e56901cc99f809eeffbb386d8bdeb24645ae0
jchahid <jalal.chahid.86@gmail.com>
2024-07-18 00:18:49 +0200

    first commit

---
+++ src/test/java/fr/capgemini/cucumberts/CucumberTsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.capgemini.cucumberts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumberTsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/fr/capgemini/cucumberts/step/Stepdefinitions.java
@@ -0,0 +1,2 @@
+package fr.capgemini.cucumberts.step;public class Stepdefinitions {
+}
